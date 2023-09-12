2cece20649804564eec50564ca8dac7c6df4a031
oaklandgit <muppethead@mac.com>
2023-09-12 13:53:05 -0700

    Testing some image manipulation.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import puppeteer from "puppeteer"
 import fs from "node:fs"
 import ansiEscapes from "ansi-escapes"
 
+const Jimp = require("jimp")
+
 const SCREENSHOT = "screenshot.png"
 
 const isUrl = (str) => {
@@ -27,24 +29,58 @@ const cli = meow("meow!", {
   },
 })
 
-const takeScreenshot = async (url, w = 1280, h = 720) => {
+const takeScreenshot = async (url, path, w = 1280, h = 720) => {
   const browser = await puppeteer.launch({ headless: "new" })
   const page = await browser.newPage()
   await page.setViewport({ width: w, height: h })
   await page.goto(url, { waitUntil: "networkidle0" })
-  await page.screenshot({ path: SCREENSHOT })
+  await page.screenshot({ path: path })
   await browser.close()
 }
 
-const renderImage = (path) => {
+const renderImage = async (path) => {
   const image = fs.readFileSync(path)
   console.log(ansiEscapes.image(image))
 }
 
-if (isUrl(cli.input.at(0))) {
-  console.log("Grabbing screenshot...")
-  await takeScreenshot(cli.input.at(0))
-  renderImage(SCREENSHOT)
-} else {
-  console.log("Please provide a valid URL")
+const prepareThenRenderImage = async (path) => {
+  Jimp.read(path)
+    .then((image) => {
+      image.flip(true, true)
+      image.write(path, () => renderImage(path))
+      console.log("Done in X seconds")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
+
+const handleUserInput = (input) => {
+  if (isUrl(input)) {
+    handleUrlRequest(input)
+  } else if (isUrl("http://" + input)) {
+    handleUrlRequest("http://" + input)
+  } else {
+    switch (input) {
+      case "A":
+        console.log("option A selected")
+        break
+
+      case "B":
+        console.log("option B selected")
+        break
+
+      default:
+        console.log("Please provide a valid URL")
+        break
+    }
+  }
+}
+
+const handleUrlRequest = (url) => {
+  console.log(`Grabbing screenshot of ${url}...`)
+  takeScreenshot(url, SCREENSHOT)
+  prepareThenRenderImage(SCREENSHOT)
+}
+
+handleUserInput(cli.input[0])
