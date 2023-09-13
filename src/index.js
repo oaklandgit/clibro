3a788792421977debb45e86b87ef3ff1c42cb795
oaklandgit <muppethead@mac.com>
2023-09-13 14:26:39 -0700

    Clean up messages.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { prepareThenRenderImage } from "./render"
 
 const SCREENSHOT = "screenshot.png"
 
-const cli = meow("meow!", {
+const cli = meow("Clibro", {
   importMeta: import.meta,
   flags: {
     width: {
@@ -27,26 +27,32 @@ const cli = meow("meow!", {
   },
 })
 
-const patterns = {
-  url: /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g,
-  code: /^[DU]$/g,
-  label: /[0-9]/g,
-}
+// const patterns = {
+//   url: /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g,
+//   code: /^[DU]$/g,
+//   label: /[0-9]/g,
+// }
+
+const urlPattern =
+  /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g
+const labelPattern = /[0-9]/g
 
 const handleUserInput = (input) => {
-  if (patterns.url.test(input)) {
+  if (urlPattern.test(input)) {
+    console.log(`Grabbing ${input}...`)
     handleUrl(input)
-  } else if (patterns.code.test(input)) {
-    console.log("That was a code!")
-  } else if (patterns.label.test(input)) {
-    console.log("That was a numeric label!")
+  } else if (input === "D") {
+    console.log("Scrolling down...")
+  } else if (input === "U") {
+    console.log("Scrolling up...")
+  } else if (labelPattern.test(input)) {
+    console.log(`Loading link labeled ${input}.`)
   } else {
-    console.log("Unknown")
+    console.log(`Invalid input: ${input}. Try 'bro --help' for help.`)
   }
 }
 
 const handleUrl = async (url) => {
-  console.log(`Grabbing screenshot of ${url}...`)
   const pageDetails = await takeScreenshot(url, SCREENSHOT, {
     w: cli.flags.width,
     h: cli.flags.height,
