679b25987773767ecdebd77d98b05b49d02ba964
oaklandgit <muppethead@mac.com>
2023-09-13 15:26:55 -0700

    A few more tweaks

--- src/render.js
+++ src/render.js
@@ -1,11 +1,13 @@
 import fs from "node:fs"
-const Jimp = require("jimp")
 import ansiEscapes from "ansi-escapes"
+const Jimp = require("jimp")
+
+const SHADOW_DIST = 2
+const LABELS = [Jimp.FONT_SANS_32_WHITE, Jimp.FONT_SANS_32_BLACK]
 
 export const prepareThenRenderImage = async (details, invert, path) => {
-  const labels = [Jimp.FONT_SANS_32_WHITE, Jimp.FONT_SANS_32_BLACK]
-  const background = await Jimp.loadFont(labels[invert ? 1 : 0])
-  const foreground = await Jimp.loadFont(labels[invert ? 0 : 1])
+  const background = await Jimp.loadFont(LABELS[invert ? 1 : 0])
+  const foreground = await Jimp.loadFont(LABELS[invert ? 0 : 1])
 
   Jimp.read(path)
     .then((image) => {
@@ -13,9 +15,9 @@ export const prepareThenRenderImage = async (details, invert, path) => {
         details.links.forEach((link, index) => {
           image.print(
             font,
-            link.left + offset * 3,
-            link.top - offset * 3,
-            index.toString()
+            link.left + offset * SHADOW_DIST,
+            link.top - offset * SHADOW_DIST,
+            `[${index}]`
           )
         })
       })
